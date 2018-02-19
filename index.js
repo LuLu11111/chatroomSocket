a3f2b9329588cfa89706da8d986d91d82c96a043
Lu Lin <lulin@lus-macbook-pro.wifi.bcit.ca>
2018-02-19 14:05:42 -0800

    add

--- index.js
+++ index.js
@@ -6,9 +6,34 @@ var io = require("socket.io")(server);
 
 var usernames =[];
 var msgs =[];
+var allRooms ={};
 
 io.on("connection", function(socket){
     console.log("User is connected");
+     socket.on("joinroom", function(data){
+        socket.emit("yourid", socket.id);
+        socket.join(data);
+        //io.emit("createimage", allusers);
+        socket.myRoom =data;
+        
+        if(!allRooms[data]){
+            allRooms[data] = [];
+        }
+        
+        
+        allRooms[data].push(socket.id);
+        io.to(data).emit("createimage", allRooms[data]);
+        
+    
+     console.log(data);
+        
+    });
+    
+    socket.on("mymove", function(data){
+        socket.to(this.myRoom).emit("usermove", data); 
+    });
+    
+    
     
     socket.on("username",function(data){
         console.log("user is giving a username:" + data);
@@ -26,8 +51,12 @@ io.on("connection", function(socket){
     
     socket.on("disconnect",function(){
         console.log("user has disconnected");
+           var index = allRooms[this.myRoom].indexOf(socket.id);
+        allRooms[this.myRoom].splice(index, 1);
+        io.to(this.myRoom).emit("createimage", allRooms[this.myRoom]);
     })
 });
+
 
 server.listen(port, (err)=>{
     if(err){
